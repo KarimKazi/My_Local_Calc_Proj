32147c07933b89d2ec6d638b24e82fb239cf7658
Karim_Ha <karim_ha@aucegypt.edu>
2021-09-10 14:58:52 +0200

    added function plus

--- calc.cpp
+++ calc.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std; 
 
+int qlus(int num1, int num2)
+{
+    return num1 + num2;
+}
+
 int main()
 {
+    int num1, num2;
+   
+    cout << "Enter first number: " << endl;
+    cin >> num1;
+    
+    cout << "Enter second number: " << endl;
+    cin >> num2;
+    
+    int answer = qlus(num1, num2);
+    
+    cout << "The result is " << answer << endl;
+    
     return 0;
 }
